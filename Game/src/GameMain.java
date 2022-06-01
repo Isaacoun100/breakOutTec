871321790ba69dd777adfb920b0115640d0f6eb2
maicolvn1399 <55199282+maicolvn1399@users.noreply.github.com>
2022-05-31 23:58:33 -0600

    [Created] Java socket client has been created

---
+++ Game/src/GameMain.java
@@ -0,0 +1,14 @@
+import com.tec.lenguajes.breakout.java_socket.GameClient;
+
+public class GameMain {
+
+    public static void main(String[] args){
+
+        System.out.println("Game Main");
+        GameClient gc = new GameClient();
+        gc.sendData("hola");
+
+    }
+
+
+}
