9449d782cf290ad4304ebdddf127c937350257eb
Isaacoun100 <Isaacoun100@estudiantec.cr>
2022-06-10 00:23:59 -0600

    Unnecessary files removed

--- Server/JSON/format.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by arch-desktop on 01/06/22.
-//
-
-#include "format.h"
--- Server/JSON/format.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by arch-desktop on 01/06/22.
-//
-
-#ifndef SERVER_FORMAT_H
-#define SERVER_FORMAT_H
-
-#endif //SERVER_FORMAT_H
