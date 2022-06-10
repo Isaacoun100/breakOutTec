a3163ce8084482897897c258f2e1d7b5bb6ca652
Naheem Johnson <naheemjohnson@estudiantec.cr>
2022-06-09 19:04:48 -0600

    Made Brick class

---
+++ Game/GameProject/src/Objects/Brick.java
@@ -0,0 +1,24 @@
+package Objects;
+
+public class Brick {
+
+    private int xValue;
+
+    private int yValue;
+
+    public int getxValue() {
+        return xValue;
+    }
+
+    public void setxValue(int xValue) {
+        this.xValue = xValue;
+    }
+
+    public int getyValue() {
+        return yValue;
+    }
+
+    public void setyValue(int yValue) {
+        this.yValue = yValue;
+    }
+}
