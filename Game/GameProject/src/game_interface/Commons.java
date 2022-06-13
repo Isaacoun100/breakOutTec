ada320e9acfa82c4bb41cb8765454c6027903bc4
Naheem Johnson <naheemjohnson@estudiantec.cr>
2022-06-13 11:42:52 -0600

    Merged branches into main

---
+++ Game/GameProject/src/game_interface/Commons.java
@@ -0,0 +1,15 @@
+package game_interface;
+
+public interface Commons {
+
+    int WIDTH = 1000;
+    int HEIGHT = 550;
+    int BOTTOM_EDGE = 540;
+    int NUMBER_OF_BRICKS = 112;
+    int INIT_PADDLE_X = 500;
+    int INIT_PADDLE_Y = 500;
+    int INIT_BALL_X = 500;
+    int INIT_BALL_Y = 500;
+    int SPEED = 1;
+
+}
