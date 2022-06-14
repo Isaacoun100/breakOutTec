ccd4a69ba8d4bafac5c2ee1efa1084c086e06682
Naheem Johnson <naheemjohnson@estudiantec.cr>
2022-06-14 16:29:56 -0600

    Added comments to Brick, GameDetails and TypeConversion class

--- Game/GameProject/src/JSON_Management/TypeConversion.java
+++ Game/GameProject/src/JSON_Management/TypeConversion.java
@@ -7,8 +7,18 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
+/**
+ * @author Naheem J.
+ * Class with static methods in charge of doing all the conversions of data, for example JSON to gameDetails object.
+ */
 public class TypeConversion {
+
+    /**
+     * @author Naheem J.
+     * Method used for converting a JSON object to a gameDetails object, which contains all of the important data regarding the game.
+     * @param jsonObject the Json containing all the game data sent from the server
+     * @return Returns the gameDetails object which was parsed from the initial json
+     */
     public static GameDetails makeGameDetailsObject(JSONObject jsonObject) {
         Brick lifepoints = new Brick();
         Brick extraBall = new Brick();
@@ -40,6 +50,12 @@ public class TypeConversion {
         return gameObject;
     }
 
+    /**
+     * @author Naheem J.
+     * Void method used for parsing a json array containing the data of the bricks to a Brick object
+     * @param jsonArray the initial json array
+     * @param brickObject the Brick object used for parsing the data
+     */
     public static void makeBrickObject(JSONArray jsonArray, Brick brickObject){
         int count = 0;
         int size;
@@ -56,6 +72,14 @@ public class TypeConversion {
             count++;
         }
     }
+
+    /**
+     * @author Naheem J.
+     * Method used for parsing a string with the structure of a json which gets sent from the server, and then gets parsed into a Json object.
+     * @param jsonString the initial json string
+     * @return Returns the json object.
+     * @throws ParseException Exception in case the parsing fails.
+     */
     public static JSONObject stringToJSONObject(String jsonString) throws ParseException {
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonObject = (JSONObject) jsonParser.parse(jsonString);
--- Game/GameProject/src/Objects/GameDetails.java
+++ Game/GameProject/src/Objects/GameDetails.java
@@ -1,5 +1,10 @@
 package Objects;
 
+/**
+ * @author Naheem J.
+ * GameDetails class used for containing all the important information regarding the game, contains the integer values,
+ * and the brick values with their corresponding coordinates
+ */
 public class GameDetails {
 
     private int greenValue;
@@ -14,6 +19,15 @@ public class GameDetails {
     private Brick moreSpeed;
     private Brick lessSpeed;
 
+    /**
+     * Constructor for the GameDetails class used for initializing the attributes
+     * @param lifepoints the life points attribute
+     * @param extraBall the extra ball attribute
+     * @param doubleRacket the doubleRacket attribute
+     * @param halfRacket the halfRacket attribute
+     * @param moreSpeed the moreSpeed attribute
+     * @param lessSpeed the lessSpeed attribute
+     */
     public GameDetails(Brick lifepoints, Brick extraBall, Brick doubleRacket, Brick halfRacket, Brick moreSpeed, Brick lessSpeed) {
         this.lifepoints = lifepoints;
         this.extraBall = extraBall;
@@ -23,59 +37,114 @@ public class GameDetails {
         this.lessSpeed = lessSpeed;
     }
 
-
+    /**
+     * Getter for greenValue variable
+     * @return greenValue
+     */
     public int getGreenValue() {
         return greenValue;
     }
 
+    /**
+     * Setter for greenValue
+     * @param greenValue
+     */
     public void setGreenValue(int greenValue) {
         this.greenValue = greenValue;
     }
 
+    /**
+     * Getter for yellowValue variable
+     * @return the yValue variable
+     */
     public int getYellowValue() {
         return yellowValue;
     }
 
+    /**
+     * Setter for yellowValue
+     * @param yellowValue the yellowValue variable
+     */
     public void setYellowValue(int yellowValue) {
         this.yellowValue = yellowValue;
     }
 
+    /**
+     * Getter for orangeValue variable
+     * @return the orangeValue variable
+     */
     public int getOrangeValue() {
         return orangeValue;
     }
 
+    /**
+     * Setter for the orangeValue variable
+     * @param orangeValue the orangeValue variable
+     */
     public void setOrangeValue(int orangeValue) {
         this.orangeValue = orangeValue;
     }
 
+    /**
+     * Getter for the redValue variable
+     * @return the redValue variable
+     */
     public int getRedValue() {
         return redValue;
     }
 
+    /**
+     * Setter for the redValue variable
+     * @param redValue the redValue variable
+     */
     public void setRedValue(int redValue) {
         this.redValue = redValue;
     }
 
+    /**
+     * Getter for the lifepoints variable
+     * @return the lifepoints variable
+     */
     public Brick getLifepoints() {
         return lifepoints;
     }
 
+    /**
+     * Getter for extraBall variable
+     * @return the extraball variable
+     */
     public Brick getExtraBall() {
         return extraBall;
     }
 
+    /**
+     * Getter for the doubleRacket variable
+     * @return the doubleRacket variable
+     */
     public Brick getDoubleRacket() {
         return doubleRacket;
     }
 
+    /**
+     * Getter for the halfRacket
+     * @return the halfRacket variable
+     */
     public Brick getHalfRacket() {
         return halfRacket;
     }
 
+    /**
+     * Getter for the moreSpeed variable
+     * @return the moreSpeed variable
+     */
     public Brick getMoreSpeed() {
         return moreSpeed;
     }
 
+    /**
+     * Getter for the lessSpeed variable
+     * @return the lessSpeed variable
+     */
     public Brick getLessSpeed() {
         return lessSpeed;
     }
